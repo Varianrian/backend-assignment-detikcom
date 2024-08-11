d662a3b2ed891dabf157ad0dd97dd879765c1b6d
Varian Avila F <varian236@gmail.com>
2024-08-11 16:32:01 +0700

    create admin command

---
+++ app/Console/Commands/CreateAdmin.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+
+class CreateAdmin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:create-admin';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create an admin user';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $name = $this->ask('What is your name?');
+        $email = $this->ask('What is your email?');
+        $password = $this->secret('What is your password?');
+        
+        $user = User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => bcrypt($password),
+            'role' => User::ROLE['admin'],
+        ]);
+
+        $this->info('Admin user created successfully');
+    }
+}
