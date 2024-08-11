8e7d246313194b314cb3f1cb75c3c8862fa08d50
Varian Avila F <varian236@gmail.com>
2024-08-11 15:52:33 +0700

    add app layout

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  @vite(["resources/css/app.css", "resources/js/app.js"])
+  <title>{{ config("app.name", "Laravel") }}</title>
+</head>
+<body class="bg-slate-950">
+@if (! Request::is("auth/*"))
+      @include("partials.navbar")
+    @endif
+
+    <main id="app" class="min-h-screen">
+      @yield("content")
+    </main>
+
+    @if (! Request::is("auth/*"))
+      @include("partials.footer")
+    @endif
+</body>
+</html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="bg-slate-900 text-white text-center py-2">
+  <p>&copy; {{ date("Y") }} {{ config("app.name", "Laravel") }}. Varian Avila Faldi</p>
+</footer>
